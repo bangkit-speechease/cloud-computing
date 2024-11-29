cdb98f447affc7f23ec9452cd7500a83a0562380
William Susilo <155814874+WilliamSusilo@users.noreply.github.com>
2024-11-29 09:17:08 +0700

    Update server.js
    Add userRoutes.js, contentRoutes.js, feedbackRoutes.js

--- API - App Engine/server.js
+++ API - App Engine/server.js
@@ -1,23 +1,19 @@
 const express = require('express')
 const authRoutes = require('./routes/auth/authRoutes.js')
-// const userRoutes = require('./routes/userProfile/userRoutes.js')
+const userRoutes = require('./routes/userProfile/userRoutes.js')
+const contentRoutes = require('./routes/content/contentRoutes.js')
+const feedbackRoutes = require('./routes/feedback/feedbackRoutes.js')
 const app = express()
 const cors = require("cors")
 
 app.use(cors())
 app.use(express.json())
-app.use(authRoutes);
-// app.use('/userProfile', userRoutes);
+
+app.use(authRoutes, userRoutes, contentRoutes, feedbackRoutes);
 
 app.get("/", (req, res) => {
     res.send("Response success!")
 })
-
-// port
-// const port = process.env.PORT || 8080;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}...`);
-// });
 
 app.listen(8080, () => {
     console.log(`Server running on port 8080...`);
