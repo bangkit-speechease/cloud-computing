d353ac9f66b2d67a7159a153a3d0526cce4cd348
IvanderKendrick <ken100840@gmail.com>
2024-12-12 10:22:55 +0700

    Deleting feedbacks test endpoint

--- API - App Engine/src/server.js
+++ API - App Engine/src/server.js
@@ -4,7 +4,6 @@ const authRoutes = require("./routes/authRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const contentRoutes = require("./routes/contentRoutes.js");
 const feedbackRoutes = require("./routes/feedbackRoutes.js");
-const feedbacksRoutes = require("./routes/feedbacksRoutes.js");
 const progressRoutes = require("./routes/progressRoutes.js");
 
 const app = express();
@@ -13,14 +12,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  authRoutes,
-  userRoutes,
-  contentRoutes,
-  feedbackRoutes,
-  feedbacksRoutes,
-  progressRoutes
-);
+app.use(authRoutes, userRoutes, contentRoutes, feedbackRoutes, progressRoutes);
 
 app.get("/", (req, res) => {
   res.send("Response success!");
